875d78380bfcf1396ed551d0c8f355eb2bd3cf48
nexoserviciosjk <ronel.rkjc@gmail.com>
2026-01-12 12:41:33 -0500

    Update platos.js

--- platos.js
+++ platos.js
@@ -12,7 +12,7 @@ const publicidadImgs = [
   "publicidad2.jpg"
 ];
 
-const publicidadTiempo = 5000;
+const publicidadTiempo = 4000;
 
 const whatsappNumero = "51999999999";
 const whatsappMensaje = "Hola, vi su menú en San Joy Lao";
